c069c09669121d61f8678fd3907e944c0cb9937e
unknown <ankurmahto1@gmail.com>
2025-06-24 00:09:32 +0100

    window creation + main loop

---
+++ engine/engine.py
@@ -0,0 +1,45 @@
+import pygame
+
+class Engine:
+
+    def __init__(self, title='Game', resolution=(1920, 1080)):
+        pygame.display.init()
+        pygame.display.set_caption(title)
+        self.resolution = resolution
+        self.screen = pygame.display.set_mode(resolution)  # test small size
+        self.clock = pygame.time.Clock()
+
+        self.dt = 0
+        self.tickrate = 60
+
+        self.running = False
+
+    def render(self):
+        self.screen.fill((0,0,0))
+        pygame.display.flip()
+
+    def run(self):
+        self.running = True
+
+        while self.running:
+
+            self.handle_events()
+            self.update()
+            self.render()
+
+            self.dt = self.clock.tick(self.tickrate)/1000
+
+        pygame.quit()
+
+
+    def handle_events(self):
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.end()
+
+
+    def update(self):
+        pass
+
+    def end(self):
+        self.running = False
