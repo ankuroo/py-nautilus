d03a39caee7a6115ee3061b9473e9b954001d7eb
ankuroo <ankurmahto1@gmail.com>
2025-06-26 23:03:49 +0100

    Add keyboard support. Add press/release/hold interactions.

--- input/input_manager.py
+++ input/input_manager.py
@@ -14,6 +14,10 @@ class InputManager:
         if self._initialized:
             return
 
+        # Keyboard
+        self.current_frame_keys = set()
+        self.last_frame_keys = self.current_frame_keys
+
         # Mouse
         self.mouse_position = (0, 0)
         self.current_mouse_buttons = (False, False, False)
@@ -34,8 +38,19 @@ class InputManager:
     def was_mouse_pressed(self, button=0):
         return self.current_mouse_buttons[button] and not self.last_mouse_buttons[button]
 
+    # Keyboard Functions
+    def was_key_pressed(self, key):
+        return key in self.current_frame_keys and key not in self.last_frame_keys
+
+    def was_key_released(self, key):
+        return key not in self.current_frame_keys and key in self.last_frame_keys
+
+    def is_key_held(self, key):
+        return key in self.current_frame_keys and key in self.last_frame_keys
+
     def update(self):
 
+        self.last_frame_keys = self.current_frame_keys.copy()
         self.last_mouse_buttons = self.current_mouse_buttons
 
         for event in pygame.event.get():
@@ -49,3 +64,8 @@ class InputManager:
                 self.current_mouse_buttons = pygame.mouse.get_pressed()
             elif event.type == pygame.MOUSEBUTTONUP:
                 self.current_mouse_buttons = pygame.mouse.get_pressed()
+
+            elif event.type == pygame.KEYDOWN:
+                self.current_frame_keys.add(event.key)
+            elif event.type == pygame.KEYUP:
+                self.current_frame_keys.discard(event.key)
