d85977dde60fe626898710c10da339ef031f49e3
ankuroo <ankurmahto1@gmail.com>
2025-06-26 23:32:39 +0100

    Add basic Controller class. Check pressed/held/released states for controller buttons.

---
+++ input/controller.py
@@ -0,0 +1,35 @@
+import pygame
+
+def detect_layout(guid: str, name: str):
+    if "PlayStation" in name or "DualSense" in name or "Sony" in name:
+        return "PlayStation"
+    elif "Xbox" in name or guid.startswith("030000005e04"):
+        return "Xbox"
+    elif "Switch" in name or "Nintendo" in name:
+        return "Nintendo"
+    elif "8BitDo" in name:
+        return "8BitDo"
+    else:
+        return "Unknown"
+
+class Controller:
+
+    def __init__(self, joystick: pygame.joystick.Joystick):
+        joystick.init()
+        self.joystick = joystick
+        self.instance_id = joystick.get_instance_id()
+        self.name = joystick.get_name()
+        self.guid = joystick.get_guid()
+        self.layout = detect_layout(self.guid, self.name)
+
+        self.current_frame_buttons = set()
+        self.last_frame_buttons = set()
+
+    def was_button_pressed(self, button):
+        return button in self.current_frame_buttons and button not in self.last_frame_buttons
+
+    def was_button_released(self, button):
+        return button not in self.current_frame_buttons and button in self.last_frame_buttons
+
+    def is_button_held(self, button):
+        return button in self.current_frame_buttons and button not in self.last_frame_buttons
